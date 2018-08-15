61cfc2e7425f06bfaf4e79015ca8a0c319fccc87
Noah <noah@gangplankhq.com>
2018-08-15 12:14:23 -0700

    finished graphing

--- public/js/index.js
+++ public/js/index.js
@@ -1,53 +1,309 @@
-var ctx = document.getElementById("bigChart").getContext('2d');
-var myChart = new Chart(ctx, {
+var apiURL = "https://api.iextrading.com/1.0/stock/";
+var apiURL2 = "/batch?types=quote,logo,news,chart&range=5d";
+var topThree = [
+]
+var botThree = [
+
+]
+axios.get(apiURL + "/market/list/gainers").then(function (res) {
+    topThree.push(res.data[0].symbol);
+    topThree.push(res.data[1].symbol);
+    topThree.push(res.data[2].symbol);
+    runTopGraph()
+}).catch(function (error) {
+    if (error) throw error
+});
+
+axios.get(apiURL + "/market/list/losers").then(function (res) {
+    botThree.push(res.data[0].symbol);
+    botThree.push(res.data[1].symbol);
+    botThree.push(res.data[2].symbol);
+    runBotGraph()
+}).catch(function (error) {
+    if (error) throw error
+});
+
+var chartConfig = {
     type: 'line',
     data: {
-        labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+        labels: [],
         datasets: [{
-            data: [1, 13, 9, 5, 2, 3],
+            data: [],
             backgroundColor: 'rgb(196, 205, 250)',
             borderColor: 'rgb(80, 104, 224)',
             lineTension: 0
         }]
     },
     options: {
-
         title: {
-            display: false
+            display: true
         },
-
         scales: {
             yAxes: [{
-                display: false,
+                display: true,
                 ticks: {
-                    beginAtZero: true
+                    beginAtZero: false
                 }
             }],
             xAxes: [{
-                display: false,
+                display: true,
             }],
 
             gridLines: {
                 display: false
             }
         },
-
         legend: {
             display: false,
         },
-
         gridLines: {
             display: false,
         }
     }
-});
+};
+var stock = "aapl";
+var chart;
 
-$(document).ready(function(){
-     // Get the height of contact 
+var topThreeOne = document.getElementById("topThreeOne").getContext('2d');
+var topThreeOne = new Chart(topThreeOne, chartConfig);
+
+var topThreeTwo = document.getElementById("topThreeTwo").getContext('2d');
+var topThreeTwo = new Chart(topThreeTwo, chartConfig);
+
+var topThreeThree = document.getElementById("topThreeThree").getContext('2d');
+var topThreeThree = new Chart(topThreeThree, chartConfig);
+
+var botThreeOne = document.getElementById("botThreeOne").getContext('2d');
+var botThreeOne = new Chart(botThreeOne, chartConfig);
+
+var botThreeTwo = document.getElementById("botThreeTwo").getContext('2d');
+var botThreeTwo = new Chart(botThreeTwo, chartConfig);
+
+var botThreeThree = document.getElementById("botThreeThree").getContext('2d');
+var botThreeThree = new Chart(botThreeThree, chartConfig);
+
+function runTopGraph() {
+    axios.get(apiURL + topThree[0] + apiURL2).then(function (res) {
+        chart = res.data.chart;
+        var percent = 0;
+        var finalPercent;
+        for (p = 0; p < chart.length; p++) {
+            console.log(chart[p].changePercent);
+            percent += chart[p].changePercent;
+        }
+        finalPercent = (percent / chart.length).toFixed(4);
+        var quote = res.data.quote;
+        $(".topOneHead").append(quote.companyName);
+        $(".topOneSub").append(quote.symbol + " ^%" + finalPercent + " average over 5 days");
+        let days1 = [
+        ]
+        for (y = 0; y < chart.length; y++) {
+            days1.push(chart[y].date);
+        }
+        var chartArr1 = [
+        ]
+        for (x = 0; x < chart.length; x++) {
+            chartArr1.push(chart[x].close);
+        }
+        console.log("1", days1, chartArr1)
+        for (i = 0; i < chart.length; i++) {
+            addData(topThreeOne, days1[i], chartArr1[i]);
+        }
+        console.log("one",topThreeOne.data.datasets)
+    }).catch(function (error) {
+        if (error) throw error
+    });
+
+
+    axios.get(apiURL + topThree[1] + apiURL2).then(function (res) {
+        chart = res.data.chart;
+        var percent = 0;
+        var finalPercent;
+        for (p = 0; p < chart.length; p++) {
+            console.log(chart[p].changePercent);
+            percent += chart[p].changePercent;
+        }
+        finalPercent = (percent / chart.length).toFixed(4);
+        var quote = res.data.quote;
+        $(".topTwoHead").append(quote.companyName);
+        $(".topTwoSub").append(quote.symbol + " ^%" + finalPercent + " average over 5 days");
+        console.log(chart);
+        let days = [
+        ]
+        for (y = 0; y < chart.length; y++) {
+            days.push(chart[y].date);
+        }
+        var chartArr = [
+        ]
+        for (x = 0; x < chart.length; x++) {
+            chartArr.push(chart[x].close);
+        }
+        console.log("2", days, chartArr);
+        for (i = 0; i < chart.length; i++) {
+            addData(topThreeTwo, days[i], chartArr[i]);
+        }
+        console.log("two",topThreeTwo.data.datasets)
+    }).catch(function (error) {
+        if (error) throw error;
+    });
+
+    axios.get(apiURL + topThree[2] + apiURL2).then(function (res) {
+        chart = res.data.chart;
+        var percent = 0;
+        var finalPercent;
+        for (p = 0; p < chart.length; p++) {
+            console.log(chart[p].changePercent);
+            percent += chart[p].changePercent;
+        }
+        finalPercent = (percent / chart.length).toFixed(4);
+        var quote = res.data.quote;
+        $(".topThreeHead").append(quote.companyName);
+        $(".topThreeSub").append(quote.symbol + " ^%" + finalPercent + " average over 5 days");
+        console.log(chart);
+        let days = [
+        ]
+        for (y = 0; y < chart.length; y++) {
+            days.push(chart[y].date);
+        }
+        var chartArr = [
+        ]
+        for (x = 0; x < chart.length; x++) {
+            chartArr.push(chart[x].close);
+        }
+        console.log("3", days, chartArr);
+        for (i = 0; i < chart.length; i++) {
+            addData(topThreeThree, days[i], chartArr[i]);
+        }
+        console.log("three",topThreeThree.data.datasets)
+    }).catch(function (error) {
+        if (error) throw error
+    });
+
+}
+function runBotGraph() {
+    axios.get(apiURL + botThree[0] + apiURL2).then(function (res) {
+        chart = res.data.chart;
+        var percent = 0;
+        var finalPercent;
+        for (p = 0; p < chart.length; p++) {
+            console.log(chart[p].changePercent);
+            percent += chart[p].changePercent;
+        }
+        finalPercent = (percent / chart.length).toFixed(4);
+        var quote = res.data.quote;
+        $(".botOneHead").append(quote.companyName);
+        $(".botOneSub").append(quote.symbol + " ^%" + finalPercent + " average over 5 days");
+        let days = [
+        ]
+        for (y = 0; y < chart.length; y++) {
+            days.push(chart[y].date);
+        }
+        var chartArr = [
+        ]
+        for (x = 0; x < chart.length; x++) {
+            chartArr.push(chart[x].close);
+        }
+        console.log("1", days, chartArr)
+        for (i = 0; i < chart.length; i++) {
+            addData(botThreeOne, days[i], chartArr[i]);
+        }
+    }).catch(function (error) {
+        if (error) throw error
+    });
+
+    axios.get(apiURL + botThree[1] + apiURL2).then(function (res) {
+        chart = res.data.chart;
+        var percent = 0;
+        var finalPercent;
+        for (p = 0; p < chart.length; p++) {
+            console.log(chart[p].changePercent);
+            percent += chart[p].changePercent;
+        }
+        finalPercent = (percent / chart.length).toFixed(4);
+        var quote = res.data.quote;
+        $(".botTwoHead").append(quote.companyName);
+        $(".botTwoSub").append(quote.symbol + " ^%" + finalPercent + " average over 5 days");
+        console.log(chart);
+        let days = [
+        ]
+        for (y = 0; y < chart.length; y++) {
+            days.push(chart[y].date);
+        }
+        var chartArr = [
+        ]
+        for (x = 0; x < chart.length; x++) {
+            chartArr.push(chart[x].close);
+        }
+        console.log("2", days, chartArr);
+        for (i = 0; i < chart.length; i++) {
+            addData(botThreeTwo, days[i], chartArr[i]);
+        }
+    }).catch(function (error) {
+        if (error) throw error;
+    });
+
+    axios.get(apiURL + botThree[2] + apiURL2).then(function (res) {
+        chart = res.data.chart;
+        var percent = 0;
+        var finalPercent;
+        for (p = 0; p < chart.length; p++) {
+            console.log(chart[p].changePercent);
+            percent += chart[p].changePercent;
+        }
+        finalPercent = (percent / chart.length).toFixed(4);
+        var quote = res.data.quote;
+        $(".botThreeHead").append(quote.companyName);
+        $(".botThreeSub").append(quote.symbol + " ^%" + finalPercent + " average over 5 days");
+        console.log(chart);
+        let days = [
+        ]
+        for (y = 0; y < chart.length; y++) {
+            days.push(chart[y].date);
+        }
+        var chartArr = [
+        ]
+        for (x = 0; x < chart.length; x++) {
+            chartArr.push(chart[x].close);
+        }
+        console.log("3", days, chartArr);
+        console.log(botThreeThree.data.datasets[0].data);
+        for (i = 0; i < chart.length; i++) {
+            addData(botThreeThree, days[i], chartArr[i]);
+        }
+    }).catch(function (error) {
+        if (error) throw error
+    });
+}
+
+function addData(graph, label, data) {
+    graph.data.labels.push(label);  
+    console.log("data data", data)
+    graph.data.datasets[0].data.push(data);
+    console.log(graph + " data: " + graph.data.datasets[0].data)
+    console.log(graph + "date:" + graph.data.labels)
+    
+    graph.update();
+
+    if (graph.data.datasets[0].data.length === 5) {
+        graph.data.datasets[0].data = [];
+    }
+    if (graph.data.labels.length === 5) {
+        graph.data.labels = [];
+    }
+    
+    // graph.data.datasets.forEach((dataset) => {
+
+    //     dataset.data.push(data);
+    // });
+}
+
+
+$(document).ready(function () {
+    // Get the height of contact 
     //  var contactHeight = $("#contact").height();
     //  $("#contact").css("height", "0px");
     //  $("#contact").show();
- 
+
     //  $("#contactLink").on("click", function () {
     //      if ($("#contact").height() === contactHeight) {
     //          $("#contact").animate({
@@ -60,9 +316,9 @@ $(document).ready(function(){
     //      }
     //  });
 
-    $("#navDropDown").on("click", function(){
+    $("#navDropDown").on("click", function () {
         var logIn = $("#logIn");
-        logIn.css("height","0px");
+        logIn.css("height", "0px");
         if (logIn.css("display") === "none") {
             logIn.css("display", "flex");
             logIn.animate({
--- public/js/stock.js
+++ public/js/stock.js
@@ -1,46 +1,46 @@
-var stock = "aapl";
+// var stock = "aapl";
 
-var apiURL = "https://api.iextrading.com/1.0/stock/";  
-var apiURL2 = "/batch?types=quote,logo,news,chart&range=1y"
+// var apiURL = "https://api.iextrading.com/1.0/stock/";  
+// var apiURL2 = "/batch?types=quote,logo,news,chart&range=1y"
 
-axios.get(apiURL + stock + apiURL2).then(function(res){
-   console.log(res.data.quote.companyName);
-   console.log(res.data.quote.open);
-   console.log(res.data.quote.high);
-   console.log(res.data.quote.low);
-   console.log(res.data.quote.iexRealtimePrice);
-   console.log(res.data.quote.change);
-   console.log(res.data.quote.changePercent);
-   console.log(res.data.quote.marketCap);
-   console.log(res.data.logo.url);
+// axios.get(apiURL + stock + apiURL2).then(function(res){
+//    console.log(res.data.quote.companyName);
+//    console.log(res.data.quote.open);
+//    console.log(res.data.quote.high);
+//    console.log(res.data.quote.low);
+//    console.log(res.data.quote.iexRealtimePrice);
+//    console.log(res.data.quote.change);
+//    console.log(res.data.quote.changePercent);
+//    console.log(res.data.quote.marketCap);
+//    console.log(res.data.logo.url);
 
-   var comp = $("<h4>").append(res.data.quote.companyName);
-   var open = $("<h4>").append(res.data.quote.open);
-   var high = $("<h4>").append(res.data.quote.high);
-   var low = $("<h4>").append(res.data.quote.low);
-   var iex = $("<h4>").append(res.data.quote.iexRealtimePrice);
-   var change = $("<h4>").append(res.data.quote.change);
-   var perc = $("<h4>").append(res.data.quote.changePercent);
-   var cap = $("<h4>").append(res.data.quote.marketCap);
-   var logo = $("<h4>").append(res.data.logo.url);
+//    var comp = $("<h4>").append(res.data.quote.companyName);
+//    var open = $("<h4>").append(res.data.quote.open);
+//    var high = $("<h4>").append(res.data.quote.high);
+//    var low = $("<h4>").append(res.data.quote.low);
+//    var iex = $("<h4>").append(res.data.quote.iexRealtimePrice);
+//    var change = $("<h4>").append(res.data.quote.change);
+//    var perc = $("<h4>").append(res.data.quote.changePercent);
+//    var cap = $("<h4>").append(res.data.quote.marketCap);
+//    var logo = $("<h4>").append(res.data.logo.url);
     
-   $("#test-content").prepend(comp);
-   $("#test-content").prepend(open);
-   $("#test-content").prepend(high);
-   $("#test-content").prepend(low);
-   $("#test-content").prepend(iex);
-   $("#test-content").prepend(change);
-   $("#test-content").prepend(perc);
-   $("#test-content").prepend(cap);
-   $("#test-content").prepend(logo);
-   var sum = 0;
-    for(var i = 0; i < res.data.chart.length; i++){
-        sum += parseInt(res.data.chart[i].close, 10);
-        console.log(res.data.chart[i].close)
-    }
-    var average = sum/res.data.chart.length;
-    console.log(res.data.chart[1])
-    console.log(res.data.chart[2])
-}).catch(function(err){
-    console.log(err)
-});
+//    $("#test-content").prepend(comp);
+//    $("#test-content").prepend(open);
+//    $("#test-content").prepend(high);
+//    $("#test-content").prepend(low);
+//    $("#test-content").prepend(iex);
+//    $("#test-content").prepend(change);
+//    $("#test-content").prepend(perc);
+//    $("#test-content").prepend(cap);
+//    $("#test-content").prepend(logo);
+//    var sum = 0;
+//     for(var i = 0; i < res.data.chart.length; i++){
+//         sum += parseInt(res.data.chart[i].close, 10);
+//         console.log(res.data.chart[i].close)
+//     }
+//     var average = sum/res.data.chart.length;
+//     console.log(res.data.chart[1])
+//     console.log(res.data.chart[2])
+// }).catch(function(err){
+//     console.log(err)
+// });
