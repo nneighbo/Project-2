fa375651b7b66a1b7207339aa8c92c78b8ceaefa
Avonlea Haymart <avhaymart@gmail.com>
2018-08-16 11:21:28 -0700

    handlebars

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,27 +4,33 @@ var isAuthenticated = require("../config/middleware/isAuthenticated");
 module.exports = function(app){
     
     app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/html/index.html"));
+        // res.sendFile(path.join(__dirname, "../public/html/index.html"));
+        res.render("index");
       });
 
       app.get("/createAccount", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/html/createAccount.html"));
+        // res.sendFile(path.join(__dirname, "../public/html/createAccount.html"));
+        res.render("createAccount");
       });
 
       app.get("/login", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/html/login.html"));
+        // res.sendFile(path.join(__dirname, "../public/html/login.html"));
+        res.render("login")
       });
 
       app.get("/news", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/html/news.html"));
+        // res.sendFile(path.join(__dirname, "../public/html/news.html"));
+        res.render("news");
       });
 
       app.get("/search", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/html/search.html"));
+        // res.sendFile(path.join(__dirname, "../public/html/search.html"));
+        res.render("search");
       });
 
     app.get("/dashboard", isAuthenticated, function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/html/dashboard.html"));
+        // res.sendFile(path.join(__dirname, "../public/html/dashboard.html"));
+        res.render("dashboard");
       });
 };
 
